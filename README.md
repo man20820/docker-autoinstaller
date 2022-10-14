ef4bda3909a72883fa48637c5819d442f506c278
Fatkhurrahman <fatkhurrahmandjogja@gmail.com>
2022-10-14 22:39:02 +0700

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 simple command to install docker
 
 ## usage
+
+```bash
+bash<(curl -s https://raw.githubusercontent.com/man20820/docker-autoinstaller/main/run.sh)
+```
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update -7
+sudo apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
@@ -12,5 +12,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
