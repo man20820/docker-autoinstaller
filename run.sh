f4026473363ecaed7a783ce56909074ac5c98340
Fatkhurrahman <fatkhurrahmandjogja@gmail.com>
2022-10-14 22:41:22 +0700

    initial commit

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update -y
-sudo apt-get install -y \
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
